5cdd1c186fe4168b87e3c089c6eaa817e27d4904
Walid Wazini <wazini25@gmail.com>
2022-09-27 16:42:31 +0800

    update product detail UI

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -16,11 +16,14 @@ const ProductDetails = (props) => {
     errorMessage: "",
   });
   const { product, loading, errorMessage } = state;
+  const [count, setCount] = useState(0);
 
-  let number;
-  const rating = Math.round(4.5);
-
-  console.log(rating);
+  const decrementCount = () => {
+    if (count > 0) setCount(count - 1);
+  };
+  const incrementCount = () => {
+    if (count < 10) setCount(count + 1);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -69,58 +72,126 @@ const ProductDetails = (props) => {
           <div
             className={`basis-3/5  w-full bg-slate-700 shadow-lg
           flex flex-col justify-start items-start
-          py-6 pl-4`}
+          py-6 pl-8`}
           >
             <div className={`basis-1/3 w-full`}>
               {/* TITLE N RATING  */}
               <div
                 className={`basis-1/2 flex flex-col justify-start items-start
-                bg-rose-500 w-full
+                 w-full
            `}
               >
                 <h1 className='text-white  text-[2.7rem]  '>{product?.name}</h1>
                 <div className={`flex gap-2 items-center w-[400px]`}>
-                  <div>⭐{product?.rating} rated</div>
-
-                  <div>sold counts</div>
+                  <div className='font-bold text-slate-300'>
+                    ⭐ {product?.rating} rated
+                  </div>
+                  {/* <div>sold counts</div> */}
                 </div>
               </div>
               {/* PRICE  */}
-              <div className='basis-1/2'>
-                <div className={`mt-10 text-[3rem] text-red-500 font-medium`}>
-                  RM {product?.price}
+              <div className='basis-1/2 '>
+                <div className={`mt-4  font-bold text-[3rem] text-slate-200`}>
+                  RM {product?.price.toFixed(2)}
                 </div>
               </div>
             </div>
             <form className={`basis-2/3 w-full flex flex-col`}>
               {/* SHIPPING  */}
-              <div
-                className={`basis-1/4 bg-rose-500 w-full flex justify-center items-start`}
-              >
-                SHIPPING
+              <div className={`basis-1/4  w-full flex h-full p-3`}>
+                <div className='basis-1/4 flex justify-start items-start text-slate-200 text-xl'>
+                  Shipping
+                </div>
               </div>
               {/* VARIANT  */}
-              <div className={`basis-1/4 flex w-full h-full bg-green-300 p-3`}>
-                <div className='basis-1/4 flex justify-start items-start text-slate-500'>
+              <div className={`basis-1/4 flex w-full h-full p-3`}>
+                <div className='basis-1/4 flex justify-start items-start text-slate-200 text-xl'>
                   Color
                 </div>
-                <div className={`grid grid-cols-4 gap-1`}>
-                  <div>Maroon</div>
-                  <div>Blue</div>
-                  <div>Sky blue</div>
-                  <div>red/black</div>
+                <div className='flex gap-3 mt-2'>
+                  <label htmlFor='color_green' className='cursor-pointer'>
+                    <input
+                      type='radio'
+                      id='color_green'
+                      name='color'
+                      className='sr-only peer'
+                      // checked
+                    />
+
+                    <span className='block w-6 h-6 bg-green-700 border border-gray-200 rounded-full ring-1 ring-offset-1 ring-transparent peer-checked:ring-gray-300'></span>
+                  </label>
+
+                  <label htmlFor='color_blue' className='cursor-pointer'>
+                    <input
+                      type='radio'
+                      id='color_blue'
+                      name='color'
+                      className='sr-only peer'
+                    />
+                    <span className='block w-6 h-6 bg-blue-700 border border-gray-200 rounded-full ring-1 ring-offset-1 ring-transparent peer-checked:ring-gray-300'></span>
+                  </label>
+                  <label htmlFor='color_red' className='cursor-pointer'>
+                    <input
+                      type='radio'
+                      id='color_red'
+                      name='color'
+                      className='sr-only peer'
+                    />
+                    <span className='block w-6 h-6 bg-red-700 border border-gray-200 rounded-full ring-1 ring-offset-1 ring-transparent peer-checked:ring-yellow-600'></span>
+                  </label>
                 </div>
               </div>
               {/* QUANTITY  */}
-              <div className={`basis-1/4 flex w-full h-full bg-rose-300 p-3`}>
-                <div className='basis-1/4 flex justify-start items-start text-slate-500'>
+              <div className={`basis-1/4 flex w-full h-full  p-3`}>
+                <div className='basis-1/4 flex justify-start items-start text-slate-200 text-xl'>
                   Quantity
+                </div>
+                <div className='flex items-center justify-center'>
+                  <div
+                    className='inline-flex shadow-md hover:shadow-lg focus:shadow-lg'
+                    // role='group'
+                  >
+                    <button
+                      onClick={decrementCount}
+                      type='button'
+                      className='text-xl font-medium rounded-l inline-block px-6 py-2.5 bg-blue-600 text-white  leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out'
+                    >
+                      -
+                    </button>
+                    <input
+                      placeholder={count}
+                      type='number'
+                      className=' w-20 text-black text-center'
+                      onKeyDown={(event) => {
+                        event.preventDefault();
+                      }}
+                    />
+                    <button
+                      onClick={incrementCount}
+                      type='button'
+                      className='text-xl font-medium rounded-r inline-block px-6 py-2.5 bg-blue-600 text-white leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out'
+                    >
+                      +
+                    </button>
+                  </div>
                 </div>
               </div>
               {/* BUTTON TO CART / BUY  */}
-              <div className='basis-1/4 bg-green-500 w-full h-full flex p-3'>
-                <button>Add To Cart 🧺</button>
-                <button>Buy Now</button>
+              <div className='basis-1/4  w-full h-full flex p-3'>
+                <div className='flex gap-3'>
+                  <button
+                    className={`p-3 border-2 rounded-sm border-red-800 bg-rose-200 text-red-800 min-w-[150px] font-medium
+                    hover:bg-white hover:border-[3px] `}
+                  >
+                    Add To Cart 🧺
+                  </button>
+                  <button
+                    className={`p-3 rounded-sm bg-red-800 text-white min-w-[150px] font-medium
+                    hover:bg-red-600 hover:font-bold `}
+                  >
+                    Buy Now
+                  </button>
+                </div>
               </div>
             </form>
           </div>
