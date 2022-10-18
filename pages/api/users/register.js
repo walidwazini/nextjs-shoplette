87b5b5200f73390952504d261f9ad51b8f17ac5e
Walid Wazini <wazini25@gmail.com>
2022-10-18 13:45:55 +0800

    user model n register route

---
+++ pages/api/users/register.js
@@ -0,0 +1,58 @@
+import nextConnect from 'next-connect';
+import bcrypt from 'bcryptjs'
+import axios from "axios";
+
+import config from "../../../utils/config";
+import client from '../../../utils/client';
+import nextConnect from "next-connect";
+
+const handler = nextConnect()
+
+handler.post(async (req, res) => {
+  const projectId = config.projectId
+  const dataset = config.dataset
+  const tokenWithWriteAccess = process.env.NEXT_PUBLIC_SANITY_TOKEN
+  // Mutations to update data in Sanity
+  const createMutations = [
+    {
+      create: {
+        _type: 'user',
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password),
+        isAdmin: false,
+      },
+    },
+  ];
+  const existUser = await client.fetch(
+    `*[_type == "user" && email == $email][0]`,
+    {
+      email: req.body.email,
+    }
+  );
+  if (existUser) {
+    return res.status(401).send({ message: 'Email aleardy exists' });
+  }
+
+  const { data } = axios.post(
+    `https://${projectId}.api.sanity.io/v1/data/mutate/${dataset}?returnIds=true`,
+    { mutations: createMutations },
+    {
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${tokenWithWriteAccess}`,
+      },
+    }
+  )
+  const userId = data.results[0].id;
+  const user = {
+    _id: userId,
+    name: req.body.name,
+    email: req.body.email,
+    isAdmin: false,
+  };
+  const token = signToken(user);
+  res.send({ ...user, token });
+})
+
+export default handler
