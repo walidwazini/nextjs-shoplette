47ff735a0297dd93a46a507d424c3c04ffc0b92c
Walid Wazini <wazini25@gmail.com>
2022-10-23 21:44:18 +0800

    cleanup acc drawer

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -7,25 +7,32 @@ import Link from "next/link";
 
 import Layout from "../../components/Layout";
 
+const profileRoute = "/user/account/profile";
+const addressesRoute = "/user/account/addresses";
+
 const User = ({ children }) => {
   const router = useRouter();
   const [accountOpen, setAccountOpen] = useState(false);
 
   const accountRouteHandler = () => {
-    setAccountOpen((prevState) => !prevState);
+    setAccountOpen(true);
     router.push("/user/account/profile");
   };
 
   const openMyAccount =
-    router.asPath === "/user/account/profile" || "/user/account/addresses";
+    router.asPath === profileRoute || router.asPath === addressesRoute;
 
   useEffect(() => {
-    if (openMyAccount) setAccountOpen(true);
+    if (openMyAccount) {
+      setAccountOpen(true);
+    } else {
+      setAccountOpen(false);
+    }
   }, [router.asPath]);
 
   const NavButton = ({ onClick, icon, title }) => (
     <button
-      className={`w-full bg-red-600 text-start flex items-center gap-2 
+      className={`w-full text-start flex items-center gap-2 
              text-xs md:text-sm  hover:text-orange-500 font-semibold text-black `}
       onClick={onClick}
     >
@@ -34,62 +41,42 @@ const User = ({ children }) => {
     </button>
   );
 
+  const AccChildBtn = ({ route, title }) => (
+    <div
+      onClick={() => router.push(route)}
+      className={`text-sm hover:cursor-pointer ${
+        router.pathname === route && `text-red-600`
+      } `}
+    >
+      {title}
+    </div>
+  );
+
   return (
     <Layout>
       <div
         className={`w-full bg-slate-600 h-[80vh] gap-1 lg:gap-3
-      flex justify-center items-center px-10 md:px-28 lg:px-40 lg:py-4 `}
+      flex justify-center items-center px-4 md:px-14 lg:px-40 py-2 lg:py-4 `}
       >
         <div className={`basis-1/5 h-full`}>
           <div
             id='drawer'
             className={`h-3/4 w-full flex flex-col bg-teal-200 p-2 `}
           >
-            <div className='basis-1/5 text-lg text-white bg-red-500 '>
+            <div className='basis-1/5 text-lg text-white '>
               <h1>User name and edit-profile</h1>
             </div>
             <div className={`basis-4/5 flex flex-col py-1 gap-2 `}>
-              {/* <button
-                className={`w-full text-start flex items-center gap-2 
-                hover:text-red-500 font-semibold text-blue-600 `}
-                onClick={accountRouteHandler}
-              >
-                <IoMdPerson className={"text-blue-600"} />
-                My Account
-              </button> */}
               <NavButton
-                onClick={accountRouteHandler}
+                // onClick={accountRouteHandler}
+                onClick={() => setAccountOpen((prevState) => !prevState)}
                 icon={<IoMdPerson className={"text-blue-600"} />}
                 title='My Account'
               />
               {accountOpen && (
                 <div className={`px-2 text-sm `}>
-                  {/* <div>Profile</div> */}
-                  {/* <Link
-                    className='active:text-red-600'
-                    href={"/user/account/profile"}
-                  >
-                    Profile 1
-                  </Link> */}
-                  <div
-                    onClick={() => router.push("/user/account/profile")}
-                    className={`text-sm hover:cursor-pointer ${
-                      router.pathname === "/user/account/profile" &&
-                      `text-red-600`
-                    } `}
-                  >
-                    Profile
-                  </div>
-                  <div>Bank n Cards</div>
-                  <div
-                    onClick={() => router.push("/user/account/addresses")}
-                    className={`text-sm hover:cursor-pointer ${
-                      router.pathname === "/user/account/addresses" &&
-                      `text-red-600`
-                    } `}
-                  >
-                    Addresses
-                  </div>
+                  <AccChildBtn route={profileRoute} title={"Profile"} />
+                  <AccChildBtn route={addressesRoute} title={"Addresses"} />
                 </div>
               )}
               <NavButton
